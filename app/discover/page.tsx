be4a6201e2a66f4b5290b401671d7dca71c618ef
Ogheneochuko <7204868+amochuko@users.noreply.github.com>
2024-05-09 13:41:39 +0100

    fix: remove merge conflict comments

--- app/discover/page.tsx
+++ app/discover/page.tsx
@@ -10,33 +10,8 @@ import {
 } from '@chakra-ui/react';
 
 const AllVideosPage = async () => {
-<<<<<<< HEAD
-  try {
-
-    /**
-     * @dev
-     * The data isn't populating because it fails to pass some `Zod` validation
-     * that the sdk is using internally (probably to checkmate the `types` that makes 
-     * up the `Asset` object)
-     */
-    const { data, error } = await livepeer.asset.getAll();
-
-    console.log('data101: ', data);
-  } catch (err: any) {
-    /**
-     * @dev
-     * The `Asset` observed at the console is a selected few that the 
-     * sdk added to the `error` object inside a `rawValue` field
-     * 
-     * @note If only the `error` object is accessed; one would see the
-     * entire `zod` validation error
-     */
-    console.log('error101: ', err.rawValue);
-  }
-=======
   const assets = await fetchAllAssets();
   console.log(assets);
->>>>>>> g2-updates-collab
 
   return (
     <main>
@@ -60,7 +35,7 @@ const AllVideosPage = async () => {
         <Flex flexDirection="column" my={10} gap={5} maxW="md">
           <Text>This is the Discover page.</Text>
           {/* <VideoCardGrid  assets={assets} />  */}
-          <button ></button>
+          <button></button>
         </Flex>
       </Box>
     </main>
