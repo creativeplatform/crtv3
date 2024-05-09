9bb6c7080d281039fe20041e09712b355d5e08e7
Ogheneochuko <7204868+amochuko@users.noreply.github.com>
2024-05-09 13:30:16 +0100

    fix: added temporal fix for Livepeer sdk

--- app/lib/sdk/livepeer/client.ts
+++ app/lib/sdk/livepeer/client.ts
@@ -1,6 +1,6 @@
 // import { Livepeer } from 'livepeer';
 import { LIVEPEER_API_KEY } from '@app/lib/utils/env';
-import { LivepeerFetcher } from '@app/lib/utils/fetchers/livepeer';
+import { LivepeerFetcher } from '@app/lib/utils/fetchers/livepeer-api';
 
 // export const livepeer = new Livepeer({
 //   apiKey: `Bearer ${process.env.LIVEPEER_API_KEY}`,
